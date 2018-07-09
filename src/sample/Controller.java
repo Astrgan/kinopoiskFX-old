e0379a94275bcd4178f88b73ef6d629b61f86285
Alexander Vasiliv <astrgan@gmail.com>
2018-07-09 11:08:38 +1000

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,70 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ImageView poster;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private TextField year;
+
+    @FXML
+    private TextField country;
+
+    @FXML
+    private TextField genres;
+
+    @FXML
+    private TextField writer;
+
+    @FXML
+    private TextField rating;
+
+    @FXML
+    private TextArea Actors;
+
+    @FXML
+    private TextField URL;
+
+    @FXML
+    private TextField URLYear;
+
+    @FXML
+    private TextField names;
+
+    @FXML
+    private Button btnSave1;
+
+    KinopoiskParser parser = new KinopoiskParser();
+
+    @FXML
+    void PrevFilm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void load(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextFilm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+}
---
+++ src/sample/KinopoiskParser.java
@@ -0,0 +1,67 @@
+package sample;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.URL;
+
+public class KinopoiskParser {
+
+    public void start (String kinoURL) {
+        Document doc = null;
+        try {
+
+            doc = Jsoup.connect(kinoURL).get();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = doc.title();
+        System.out.println("Title : " + title);
+
+
+        Element table = doc.select("table").first();
+
+        Elements rows = table.select("tr");
+
+        for (int i = 0; i < rows.size(); i++) {
+            Element row = rows.get(i); //по номеру индекса получает строку
+            Elements cols = row.select("td");// разбиваем полученную строку по тегу  на столбы
+            System.out.print(cols.get(0).text() + ": ");// первый столбец
+            System.out.print(cols.get(1).text());
+
+            System.out.println();
+        }
+
+
+        System.out.println(doc.select("div[itemprop=description]").get(0).text());
+
+        System.out.println(doc.select("span[class=rating_ball]").get(0).text());
+
+        Element link = doc.select("link[rel=\"image_src\"]").first();
+        String relHref = link.attr("href");
+        System.out.println(relHref);
+        getImage(relHref);
+    }
+
+    public void getImage(String strURL){
+        try{
+
+            URL url = new URL(strURL);
+            InputStream in = url.openStream();
+            OutputStream out = new BufferedOutputStream(new FileOutputStream("image.jpg"));
+
+            for (int b; (b = in.read()) != -1;) {
+                out.write(b);
+            }
+            out.close();
+            in.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
